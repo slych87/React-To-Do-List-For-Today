586fc4c30e0f9a2c86c7b4e1f23aa1cc3aa59279
Mateusz Slyszewski <mateuszslyszewski@gmail.com>
2020-06-27 10:57:36 +0200

    props

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Countdown from './Countdown'
+
+const App = () => (
+    <div>
+        <Countdown name="śniadanie" time="07:00" />
+        <Countdown name="obiad" time="15:00" />
+    </div>
+);
+
+export default App;
---
+++ src/Countdown.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Countdown = (props) => (
+    <div className="countdown">
+        <strong>{props.name}</strong> {props.time}
+    </div>
+);
+
+export default Countdown;
