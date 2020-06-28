0892278849445902c0f7ce15c6a381d5bd823d00
Mateusz Slyszewski <mateuszslyszewski@gmail.com>
2020-06-28 08:39:57 +0200

    buttons onSave onClick

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Countdown from './Countdown'
+import "./App.css";
+import EditEvent from "./EditEvent"
 
-const App = () => (
-    <div>
-        <Countdown name="śniadanie" time="07:00" />
-        <Countdown name="obiad" time="15:00" />
-    </div>
-);
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            events: [
+                { id: 0, name: "śniadanie", time: "07:00" },
+                { id: 1, name: "obiad", time: "15:00" },
+                { id: 2, name: "kolacja", time: "19:00" },
+            ]
+        }
+    }
+    render() {
+        const events = this.state.events.map(el => {
+            return <Countdown key={el.id} name={el.name} time={el.time} />
+        })
+        return (
+            <div className="app">
+                {events}
+                <EditEvent onSave={() => alert("A")} />
+            </div>
+        );
+    }
+}
+
 
 export default App;
